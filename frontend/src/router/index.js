05ffb258a1e11727c5a2ff2b54e0b014d2384a49
giuliomuraro <giulio.muraro@studenti.unitn.it>
2024-02-08 15:54:32 +0100

    Fixed some problems with the Schemes of mongoDB, backend for reports ended

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,7 +4,7 @@ import SignUpView from '../views/SignUpView.vue'
 import LoginView from '../views/LoginView.vue'
 import AdminAccountView from '../views/AdminAccountView.vue'
 import ProfileView from '../views/ProfileView.vue'
-import PrenotazioneView from '../views/PrenotazioneView.vue'
+import BookingView from '../views/BookingView.vue'
 import NotFoundView from '../views/NotFoundView'
 import ConfermaView from '../views/ConfermaView'
 import DashBoardView from '../views/DashBoardView'
@@ -24,7 +24,7 @@ const routes = [
   {
     path: '/prenotazione',
     name: 'Prenotazione',
-    component: PrenotazioneView,     
+    component: BookingView,     
   },
   {
     path: '/conferma',
--- frontend/src/views/BookingView.vue
+++ frontend/src/views/BookingView.vue
@@ -1,17 +1,26 @@
 <template>
-    <div >
-        <NavBar/>
-    </div>
-  </template>
-  
-  <script>
-  // @ is an alias to /src
-  import NavBar from '@/components/NavBar.vue'
-  
-  export default {
-    name: 'BookingView',
-    components: {
-      NavBar, 
+  <div >
+    <NavBar />
+    <PrenotazioneForm />
+  </div>
+</template>
+
+<script>
+import PrenotazioneForm from '@/components/PrenotazioneForm.vue';  
+import NavBar from '@/components/NavBar.vue';
+import store from '@/store/index';
+import router from '@/router';
+
+export default {
+  components: {
+    PrenotazioneForm,      
+    NavBar
+  },
+  mounted(){
+    if(!store.state.token){        
+    router.replace({name: "Login"})      
+    //console.log("utente non autenticato")
     }
-  }
-  </script>
+  },
+};
+</script>
